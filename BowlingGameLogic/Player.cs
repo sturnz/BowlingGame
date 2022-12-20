fb6431e1be847692a73da8bb7d8033a8c1f93f26
Jasmin <User@DESKTOP-23QVBVH>
2022-12-20 11:44:40 +0100

    Index eingeführt um Punktekarte nicht immer an der selben Stelle zu befüllen

--- BowlingGameLogic/Player.cs
+++ BowlingGameLogic/Player.cs
@@ -3,10 +3,12 @@
     public class Player
     {
         public int[] scoreCard = new int[21];
+        int numberOfRoll = 0;
 
         public void Roll(int fallenPins)
         {
-            scoreCard[0] = fallenPins;
+            scoreCard[numberOfRoll] = fallenPins;
+            numberOfRoll++;
         }
     }
 }
