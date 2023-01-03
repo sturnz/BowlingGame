62438feefa74be3a7eaff399bd8132cf3347d500
Jasmin <spock@DESKTOP-KN9AIC0>
2023-01-03 23:37:53 +0100

    Neuer Test, neue Klasse Game um gewüschte Anzahl an Spielern zu instanzieren

---
+++ BowlingGameLogic/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameLogic
+{
+    public class Game
+    {
+        public void Run(int numberOfPlayers)
+        {
+            for (int index = 0; index < numberOfPlayers; index++)
+            {
+                Player player = new Player();
+            }
+        }
+    }
+}
---
+++ BowlingGameTests/GameTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGameLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameLogic.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void RunAGame()
+        {
+            Game game = new Game();
+
+            Assert.IsNotNull(game);
+        }
+    }
+}
