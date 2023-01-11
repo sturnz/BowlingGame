d94363bf11d3409a88bdfa024f520d951ab79011
Jasmin <spock@DESKTOP-KN9AIC0>
2023-01-11 21:14:29 +0100

    Konsolenspiel hinzugefügt

---
+++ BowlingGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using BowlingGameLogic;
+
+namespace BowlingGameConsole
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<Player>    listOfPlayers   = new();
+            Game            game            = new();
+
+            Console.WriteLine("How many players are playing?");
+            game.Run(Convert.ToInt32(Console.ReadLine()));
+
+            
+            foreach (var player in game.listOfPlayers)
+            {
+                Console.WriteLine($"Total points of Player {listOfPlayers.IndexOf(player)+2}: " + player.GetTotalScore());
+            }
+        }
+    }
+}
